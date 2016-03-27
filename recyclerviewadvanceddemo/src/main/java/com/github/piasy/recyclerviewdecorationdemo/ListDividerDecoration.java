ebefa0196c8e1c3f0307b84b5ce18ab687e184f4
Piasy <xz4215@gmail.com>
2016-03-27 12:48:04 +0800

    [RecyclerViewAdcancedDemo] test onDraw

--- recyclerviewadvanceddemo/src/main/java/com/github/piasy/recyclerviewdecorationdemo/ListDividerDecoration.java
+++ recyclerviewadvanceddemo/src/main/java/com/github/piasy/recyclerviewdecorationdemo/ListDividerDecoration.java
@@ -32,6 +32,7 @@ import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 
 public class ListDividerDecoration extends RecyclerView.ItemDecoration {
 
@@ -57,7 +58,19 @@ public class ListDividerDecoration extends RecyclerView.ItemDecoration {
 
     @Override
     public void onDraw(Canvas c, RecyclerView parent) {
-        // draw nothing
+        final int left = parent.getPaddingLeft() - 100;
+        final int right = parent.getWidth() - parent.getPaddingRight() - 100;
+
+        final int childCount = parent.getChildCount();
+        for (int i = 0; i < childCount; i++) {
+            final View child = parent.getChildAt(i);
+            final RecyclerView.LayoutParams params =
+                    (RecyclerView.LayoutParams) child.getLayoutParams();
+            final int top = child.getBottom() + params.bottomMargin - 25;
+            final int bottom = top + 50;
+            mDivider.setBounds(left, top, right, bottom);
+            mDivider.draw(c);
+        }
     }
 
     @Override
