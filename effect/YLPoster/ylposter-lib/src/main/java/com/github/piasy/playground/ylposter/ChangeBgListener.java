acdc5c9a9fc7b786a5385a170d72fa5658250211
Piasy <xz4215@gmail.com>
2017-01-19 22:02:57 +0800

    clean graduated code

--- effect/YLPoster/ylposter-lib/src/main/java/com/github/piasy/playground/ylposter/ChangeBgListener.java
+++
@@ -1,9 +0,0 @@
-package com.github.piasy.playground.ylposter;
-
-/**
- * Created by Piasy{github.com/Piasy} on 24/11/2016.
- */
-
-public interface ChangeBgListener {
-    void changeBg();
-}
--- try/RgbYuvConverter/test/dump.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-adb pull /sdcard/org_640_480.yuv
-adb pull /sdcard/out_640_384.yuv
